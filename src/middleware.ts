ffbfed7dfdc7c09cddc1063ef783ccab287a6db5
Aryan <aryan.gurung683@gmail.com>
2024-09-18 21:42:03 +0545

    seond commit

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,15 +1,11 @@
 import { authMiddleware } from "@kinde-oss/kinde-auth-nextjs/server";
-import { NextApiRequest, NextApiResponse } from "next";
+import { NextApiRequest } from "next";
 
 export const config = {
   matcher: ["/dashboard/:path*", "/auth-callback"],
 };
 
-const handler = (
-  req: NextApiRequest,
-  res: NextApiResponse,
-  next: () => void
-) => {
+const handler = (req: NextApiRequest) => {
   console.log("Middleware applied to:", req.url);
   authMiddleware(req);
 };
