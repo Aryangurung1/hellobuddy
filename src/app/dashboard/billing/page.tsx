a7adafa7eb9f34d52f787634152b93475179df27
aryangurung <aryan.gurung683@gmail.com>
2024-09-19 21:30:57 +0545

    alldoone good work

--- src/app/dashboard/billing/page.tsx
+++ src/app/dashboard/billing/page.tsx
@@ -1,39 +1,21 @@
-// import BillingForm from "@/components/BillingForm";
-// import { getUserSubscriptionPlan } from "@/lib/stripe";
-// import { redirect } from "next/navigation";
-
-// const Page = async () => {
-//   try {
-//     const subscriptionPlan = await getUserSubscriptionPlan();
-
-//     // Check if subscriptionPlan or its id is null/undefined
-//     if (!subscriptionPlan) {
-//       console.error("Invalid subscription plan data:", subscriptionPlan);
-//       throw new Error("Subscription plan not found or invalid data returned.");
-//     }
-
-//     return <BillingForm subscriptionPlan={subscriptionPlan} />;
-//   } catch (error) {
-//     redirect("/sign-in");
-//   }
-// };
-
-// export default Page;
-
 import BillingForm from "@/components/BillingForm";
 import { getUserSubscriptionPlan } from "@/lib/stripe";
-import { redirect } from "next/navigation"; // To redirect user
+import { redirect } from "next/navigation";
 
 const Page = async () => {
-  const subscriptionPlan = await getUserSubscriptionPlan();
+  try {
+    const subscriptionPlan = await getUserSubscriptionPlan();
 
-  // If subscriptionPlan is null or undefined, handle it
-  if (!subscriptionPlan) {
-    console.error("No subscription plan found for the user.");
-    return redirect("/sign-in"); // Redirect to the sign-in page or an error page
+    // Check if subscriptionPlan or its id is null/undefined
+    if (!subscriptionPlan) {
+      console.error("Invalid subscription plan data:", subscriptionPlan);
+      throw new Error("Subscription plan not found or invalid data returned.");
+    }
+
+    return <BillingForm subscriptionPlan={subscriptionPlan} />;
+  } catch (error) {
+    console.log(error);
   }
-
-  return <BillingForm subscriptionPlan={subscriptionPlan} />;
 };
 
 export default Page;
