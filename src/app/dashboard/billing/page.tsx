55c4749c26a4a284fd6cdc5002ff9055b710f1e1
Aryan <aryan.gurung683@gmail.com>
2024-09-19 17:13:55 +0545

    all done

--- src/app/dashboard/billing/page.tsx
+++ src/app/dashboard/billing/page.tsx
@@ -1,21 +1,32 @@
+// import BillingForm from "@/components/BillingForm";
+// import { getUserSubscriptionPlan } from "@/lib/stripe";
+// import { redirect } from "next/navigation";
+
+// const Page = async () => {
+//   try {
+//     const subscriptionPlan = await getUserSubscriptionPlan();
+
+//     // Check if subscriptionPlan or its id is null/undefined
+//     if (!subscriptionPlan) {
+//       console.error("Invalid subscription plan data:", subscriptionPlan);
+//       throw new Error("Subscription plan not found or invalid data returned.");
+//     }
+
+//     return <BillingForm subscriptionPlan={subscriptionPlan} />;
+//   } catch (error) {
+//     redirect("/sign-in");
+//   }
+// };
+
+// export default Page;
+
 import BillingForm from "@/components/BillingForm";
 import { getUserSubscriptionPlan } from "@/lib/stripe";
-import { redirect } from "next/navigation";
 
 const Page = async () => {
-  try {
-    const subscriptionPlan = await getUserSubscriptionPlan();
+  const subscriptionPlan = await getUserSubscriptionPlan();
 
-    // Check if subscriptionPlan or its id is null/undefined
-    if (!subscriptionPlan) {
-      console.error("Invalid subscription plan data:", subscriptionPlan);
-      throw new Error("Subscription plan not found or invalid data returned.");
-    }
-
-    return <BillingForm subscriptionPlan={subscriptionPlan} />;
-  } catch (error) {
-    redirect("/sign-in");
-  }
+  return <BillingForm subscriptionPlan={subscriptionPlan} />;
 };
 
 export default Page;
