cd7c641580324302c00fd7337dc29d9495c11414
Aryangurung1 <kamina5432167890@gmail.com>
2024-09-20 12:00:39 +0545

    all done gooluck

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -19,7 +19,7 @@ const Navbar = async () => {
       <MaxWidthWrapper>
         <div className="flex h-14 items-center justify-between border-b border-zinc-200">
           <Link href="/" className="flex z-40 font-semibold">
-            <span>HelloBuddy how are you</span>
+            <span>HelloBuddy superman</span>
           </Link>
 
           <MobileNav isAuth={!!user} />
